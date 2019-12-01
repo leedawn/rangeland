fbb0671e8a418ad7f451a3c39fc007abadfdebe7
leedawn <1558449520@qq.com>
2019-12-02 00:48:03 +0800

    add table

---
+++ src/views/FieldsDef.js
@@ -0,0 +1,25 @@
+import VuetableFieldHandle from 'vuetable-2/src/components/VuetableFieldHandle.vue'
+
+export default [
+  {
+    name: VuetableFieldHandle
+  },
+  {
+    name: 'title',
+    // title: '<span class="orange glyphicon glyphicon-user"></span> Full Name',
+    sortField: 'title'
+  },
+  {
+    name: 'bak2',
+    sortField: 'bak2'
+  },
+  'bak1'
+//   'nickname',
+//   {
+//     name: 'gender',
+//     formatter: (value) => {
+//       return value === 'M' ? 'Male' : 'Female'
+//     }
+//   }
+//   'actions'
+]
